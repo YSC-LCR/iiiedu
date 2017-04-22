030700b06123055f08d579d4d2588714c4e47c43
orgin <https://github.com/YSC-LCR/iiiedu.git>
2017-04-22 22:57:33 +0800

    加入DemoServlet

---
+++ GitHub/src/main/java/ch01/ex02/FirstServlet.java
@@ -0,0 +1,23 @@
+package ch01.ex02;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/FirstServlet.do")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("這是doGet()方法!!");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ GitHub/src/main/webapp/default.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>這是第一個JSP測試網頁</title>
+</head>
+<body>
+	<a href="FirstServlet.do">測試</a>
+</body>
+</html>
